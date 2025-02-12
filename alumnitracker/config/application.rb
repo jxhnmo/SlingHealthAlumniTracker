28b0e934c1277f8e064e1942dc3df3eb29263703
John Mo <peikaimo@gmail.com>
2025-02-12 00:03:20 -0600

    edit application.rb to setup for api only mode for nextjs

--- alumnitracker/config/application.rb
+++ alumnitracker/config/application.rb
@@ -11,6 +11,13 @@ module Alumnitracker
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.api_only = true
+
+    config.middleware.delete ActionDispatch::Session::CookieStore
+    config.middleware.delete ActionDispatch::Flash
+    config.middleware.delete Rack::MethodOverride
+    config.middleware.delete ActionDispatch::RequestId
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
