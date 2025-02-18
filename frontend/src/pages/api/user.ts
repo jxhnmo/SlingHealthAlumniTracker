f1188771ee63a7fcbb2883bc52d30047a8ba7d8c
Rkha21 <rkha02134@gmail.com>
2025-02-17 21:45:38 -0600

    Changed user.ts

--- frontend/src/pages/api/user.ts
+++ frontend/src/pages/api/user.ts
@@ -1,5 +1,6 @@
 export const fetchUsers = async () => {
-  const response = await fetch('/userIndex'); 
+  const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://alumnitrackertest-958bb6be1026.herokuapp.com";
+  const response = await fetch(`${API_URL}/users`);
   if (!response.ok) throw new Error('Failed to fetch users');
   return response.json();
 };
