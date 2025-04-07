36435e8241b08fecc86847667f67978d78a94708
Baconsizzle1738 <zhenghao@tamu.edu>
2025-04-07 18:22:00 -0500

    no jwt

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,11 +2,11 @@ class Admin < ApplicationRecord
     devise :omniauthable, omniauth_providers: [:google_oauth2]
     def self.from_google(email:, full_name:, uid:, avatar_url:)
         create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
-      end
-    def self.generate_jwt(admin)
-        payload = { admin_id: admin.id, email: admin.email }
-        JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
-      end
+    end
+    # def self.generate_jwt(admin)
+    #     payload = { admin_id: admin.id, email: admin.email }
+    #     JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
+    # end
     
 
   end
