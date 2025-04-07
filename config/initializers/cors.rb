ada596027648e62eaee655faabe66a1ea6854853
Baconsizzle1738 <zhenghao@tamu.edu>
2025-04-07 17:10:56 -0500

    no localhost on cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4000', 'https://alumni-tracker-sprint3-84062556e525.herokuapp.com' 
+    origins 'https://alumni-tracker-sprint3-84062556e525.herokuapp.com' 
     resource '*',
              headers: :any,          
              methods: %i[get post put delete options head]
