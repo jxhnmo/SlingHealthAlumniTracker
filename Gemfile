93855c82c898f7b12050db34ef74867efc275847
Rkha21 <rkha02134@gmail.com>
2025-04-07 17:40:22 -0500

    Changed admin.rb

--- Gemfile
+++ Gemfile
@@ -63,4 +63,5 @@ gem 'devise'
 gem 'omniauth'
 gem 'omniauth-google-oauth2'
 gem 'omniauth-rails_csrf_protection'
+gem 'jwt'
 
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,14 +1,17 @@
 class Admin < ApplicationRecord
     devise :omniauthable, omniauth_providers: [:google_oauth2]
-    def self.from_google(email:, full_name:, uid:, avatar_url:)
-        create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
-      end
-    def self.generate_jwt(admin)
-        payload = { admin_id: admin.id, email: admin.email }
-        JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
-      end
-    
-
-  end
   
+    def self.from_google(email:, full_name:, uid:, avatar_url:)
+      find_or_create_by!(email: email) do |admin|
+        admin.uid = uid
+        admin.full_name = full_name
+        admin.avatar_url = avatar_url
+      end
+    end
+  
+    def self.generate_jwt(admin)
+      payload = { admin_id: admin.id, email: admin.email, exp: 24.hours.from_now.to_i }
+      JWT.encode(payload, Rails.application.secret_key_base, 'HS256')
+    end
+  end
   
