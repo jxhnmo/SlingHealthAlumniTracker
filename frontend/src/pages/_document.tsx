25ee14124747b6a2ff2f1efd6e20bb75699a0e0c
Baconsizzle1738 <zhenghao@tamu.edu>
2025-04-10 17:04:14 -0500

    to raiksl

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,12 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  const FILE_UPLOAD_API = process.env.SIMPLE_FILE_UPLOAD_API;
-  const srcurl = "https://app.simplefileupload.com/buckets/" + FILE_UPLOAD_API + ".js"
+  // const FILE_UPLOAD_API = process.env.SIMPLE_FILE_UPLOAD_API;
+  // const srcurl = "https://app.simplefileupload.com/buckets/" + FILE_UPLOAD_API + ".js"
   return (
     <Html lang="en">
       <Head>
-        <script src="https://app.simplefileupload.com/buckets/ee5fd30cbfd7939d9e52b522e52a6775.js"></script>
+        {/* <script src="https://app.simplefileupload.com/buckets/ee5fd30cbfd7939d9e52b522e52a6775.js"></script> */}
         {/* <script src={srcurl}></script> */}
       </Head>
       {/* <Head /> */}
