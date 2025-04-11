3f5fb2fb9a7138d3be4d91c22daa0811c44c44d6
Karthiknakka1 <Karthiknakka1@gmail.com>
2025-04-10 22:34:06 -0500

    fix link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4000', 'https://alumni-tracker-sprint2-d1ab480922a9.herokuapp.com' 
+    origins 'http://localhost:4000', 'https://alumni-tracker-sprint2-d1ab480922a9.herokuapp.com', 'https://alumni-tracker-sprint3-84062556e525.herokuapp.com'
     resource '*',
-             headers: :any,          
+             headers: :any,
              methods: %i[get post put delete options head]
   end
 end
-
-
