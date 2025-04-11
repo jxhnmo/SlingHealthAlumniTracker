fb1aa8140c566f0969db632e73d9c76581ec1122
Arivera919 <Arivera919@outlook.com>
2025-04-11 00:23:53 -0500

    fixed teams

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
 
   has_many :achievements, dependent: :destroy
   has_many :contact_methods, dependent: :destroy
-  has_one :team, dependent: :destroy
+  has_one :teams_user, dependent: :destroy
+  has_one :team, through: :teams_user
   accepts_nested_attributes_for :achievements, allow_destroy: true
   accepts_nested_attributes_for :contact_methods, allow_destroy: true
   accepts_nested_attributes_for :team, update_only: true
